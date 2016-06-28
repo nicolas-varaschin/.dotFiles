381bef006f887b82327dcd2f3b8bafdad25abffc
nvaraschin <nicovaras22@gmail.com>
2016-06-28 09:00:20 -0300

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,24 +1,29 @@
 function fish_prompt
     set_color -o 4B7D14
     echo -n '['
-    set_color normal 
-    set_color C9B963 
+    set_color normal
+    set_color C9B963
     echo -n (basename (pwd))
     set_color -o 4B7D14
     echo -n ']'
+    if git rev-parse --is-inside-work-tree > /dev/null 2>&1
+      echo -n '('
+      echo -n (git rev-parse --abbrev-ref HEAD)
+      echo -n ')'
+    end
     set_color -o 168FC9
     echo -n (whoami)
     set_color -o purple
     echo " ➜ "
 end
-function cdd
-	cd $argv; and ls -al
+function vf
+        cd $argv; and ls -al
 end
 
-function las
-	command ls -al --color=auto
+function la
+        ls -al --color=auto
 end
 
-alias ls='las'
+
 alias cat='scat'
 export GIT_EDITOR="subl --wait"
